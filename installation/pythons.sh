dce473abd99bd15aa3f59ef4107c7b47acbd9fb1
shak48 <rumie.kabir@gmail.com>
2020-09-05 08:29:32 -0600

    First Commit

---
+++ installation/pythons.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 'Installing Python Machine Learning Libraries by shak48'
+sudo pip3 install numpy
+sudo pip3 install pandas
+sudo pip3 install scikit-learn
+
+
+
+
---
+++ installation/rstudio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo '####Installing Language R and RStudio Basic####'
+sudo apt update -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+sudo apt-get install r-base-dev
+
+
+
+
+
